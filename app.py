43fb6a2edd739c4fa0ccd34dae601cca691e4ff1
Vitoriox <victorfla.personal@gmail.com>
2021-09-27 20:54:58 +0200

    somef integration test

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from somef.cli import cli_get_data
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'SOMEF WebApp'
+
+    return cli_get_data(0.8, True, repo_url='https://github.com/dgarijo/Widoco/')
 
 
 if __name__ == '__main__':

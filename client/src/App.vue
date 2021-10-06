205c25fc3a2cd88efafa214db7b89e6008a1e55c
Vitoriox <victorfla.personal@gmail.com>
2021-10-06 17:48:27 +0200

    Vuetify integration

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Home></Home>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-spacer></v-spacer>
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Home/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Home from './components/Home.vue'
+import Home from './components/Home';
+
 export default {
   name: 'App',
   components: {
-    Home
-  }
-}
-</script>
+    Home,
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,40 +1,61 @@
 <template>
-<p>{{data}}</p>
-
+  <v-container>
+    <v-card>
+      <v-card-title>
+        SOMEF WEBAPP
+      </v-card-title>
+      <v-container>
+        <v-row justify="center" align-content="center">
+          <v-col>
+            <v-text-field
+                placeholder="widoco"
+                :outlined="true"
+            >
+            </v-text-field>
+          </v-col>
+          <v-col cols="2">
+            <v-text-field
+                placeholder="Thressold"
+                :value="0.8"
+                :disabled="true"
+                type='number'
+                :outlined="true"
+            >
+            </v-text-field>
+          </v-col>
+          <v-col>
+            <v-btn
+                color="primary"
+                @click="getMetadata"
+            >
+              SUBMIT
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-import somefWebAppService from "@/service/somefWebAppService";
-import axios from 'axios';
+  import Axios from "axios";
 
-export default {
-  data(){
-    return{
-          data: null
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+
+    }),
+    methods:{
+      getMetadata() {
+        Axios.get("http://127.0.0.1:5000/test")
+            .then(response => {
+              console.log(response.data)
+            })
+            .catch(error => {
+              console.log(error)
+            })
+      }
     }
-  },
-  methods:{
-    getTest(){
-      somefWebAppService.getTest()
-          .then((response) => {
-            this.data = response.data
-          })
-          .catch(error => {
-            console.log(error);
-          });
-    },
-    getTest2(){
-      axios.get("http://127.0.0.1:5000/test")
-          .then((response) => {
-            this.data = response.data
-          })
-          .catch(error => {
-            console.log(error);
-          });
-    }
-  },
-  created() {
-    this.getTest2()
   }
-}
 </script>

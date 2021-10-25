a82afdc64a410990f5d3105c9b429abf9bfa47b6
Vitoriox <victorfla.personal@gmail.com>
2021-10-25 23:07:44 +0200

    Added vue router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,35 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <v-spacer></v-spacer>
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <Home/>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Home from './views/Home';
 
 export default {
   name: 'App',
-  components: {
-    Home,
-  },
 
   data: () => ({
     //

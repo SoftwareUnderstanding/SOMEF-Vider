dba6a1a0e0e9e754aec84e0e6833621dfb2e4b42
Vitoriox <victorfla.personal@gmail.com>
2021-11-22 20:43:58 +0100

    Updated dependencies Added download method

--- server/app.py
+++ server/app.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, render_template, send_file, send_from_directory
-from somef.cli import cli_get_data
-from flask_cors import CORS
+from flask import Flask, request, send_file, send_from_directory
+from somef.cli import cli_get_data, run_cli
 
 app = Flask(__name__)
-CORS(app)
 
 
 @app.route('/')
@@ -36,15 +34,43 @@ def get_metadata():
         return "GitHub URL is not valid", 400
 
     metadata = cli_get_data(threshold, ignore_classifiers, repo_url=url)
+    path = 'server/generated-files/metadata'
+    run_cli(threshold=threshold, ignore_classifiers=ignore_classifiers, repo_url=url,
+            output=path, graph_out=path, codemeta_out=path)
     if metadata is None:
         return "GitHub repository is not valid", 400
     else:
         return metadata
 
 
+@app.route('/download')
+def download_metadata():
+    file_type = parse_file_type(request.args.get('fileType'))
+    if file_type is None:
+        return "Invalid file type for download", 400
+
+    path = 'generated-files/'
+    if file_type == 'json':
+        try:
+            return send_file(path+'metadata.json', as_attachment=True)
+        except Exception:
+            return "Metadata file not generated", 400
+
+    elif file_type == 'codemeta':
+        try:
+            return send_file('metadata', as_attachment=True)
+        except Exception:
+            return "Metadata file not generated", 400
+
+
 @app.route('/test')
 def test():
-    return cli_get_data(0.8, False, repo_url='https://github.com/KnowledgeCaptureAndDiscovery/somef')
+    repo_url = 'https://github.com/KnowledgeCaptureAndDiscovery/somef'
+    path = 'server/generated-files/'
+    run_cli(threshold=0.8, ignore_classifiers=False, repo_url=repo_url,
+            output=path+'metadata.json', graph_out=path+'metadata-turtle', codemeta_out=path+'metadata-codemeta')
+    return send_file('generated-files/metadata.json', as_attachment=True)
+    # return cli_get_data(0.8, False, repo_url=repo_url)
 
 
 def parse_threshold(value):
@@ -63,6 +89,17 @@ def parse_ignore_classifiers(value):
         return True
     elif value == 'false':
         return False
+    else:
+        return None
+
+
+def parse_file_type(value):
+    if value == 'json':
+        return 'json'
+    elif value == 'codemeta':
+        return 'codemeta'
+    elif value == 'turtle':
+        return 'turtle'
     else:
         return None
 

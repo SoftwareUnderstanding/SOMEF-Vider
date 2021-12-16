5696579459f08be4e5e400c445c312df5657eecb
Vitoriox <victorfla.personal@gmail.com>
2021-12-16 13:52:54 +0100

    Form component cleanup & issue #28: Added loading dialog

---
+++ client/src/components/LoadingDialog.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-dialog
+        v-model="value"
+        persistent
+        width="300"
+    >
+      <v-card>
+        <v-card-text class="text-center">
+          We are analyzing your repository.
+          <br>Time may vary depending on its size.
+          <br><br>Please be patient.
+          <v-progress-linear
+              indeterminate
+              color="blue"
+              class="mb-0"
+          ></v-progress-linear>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+  props:{
+    value:{
+      type: Boolean,
+      default: false,
+    }
+  },
+
+}
+</script>
---
+++ client/src/components/SomefForm.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-form ref="form">
+    <v-row justify="space-between">
+
+      <v-col align-self="center">
+        <v-text-field
+            placeholder="https://github.com/KnowledgeCaptureAndDiscovery/somef/"
+            :outlined="true"
+            v-model="url"
+            :rules="rules.urlRules"
+            required
+            label="GitHub URL"
+        >
+        </v-text-field>
+      </v-col>
+
+      <v-col cols="2" align-self="center" md="auto">
+        <v-text-field
+            placeholder="Threshold"
+            :max="1"
+            :min="0"
+            :step="0.1"
+            required
+            v-model="threshold"
+            :rules="rules.thresholdRules"
+            type='number'
+            :outlined="true"
+            label="Threshold"
+        >
+        </v-text-field>
+      </v-col>
+
+      <v-col cols="2" md="auto">
+        <v-btn
+            color="primary"
+            @click="submit"
+        >
+          SUBMIT
+        </v-btn>
+      </v-col>
+
+    </v-row>
+  </v-form>
+</template>
+
+<script>
+
+export default {
+  name: 'SomefForm',
+
+  data: () => ({
+    url: null,
+    threshold: null,
+    ignoreClassifiers: false,
+    rules:{
+      urlRules: [
+        v => !!v || 'GitHub URL is required',
+        //v => /^(https?:\\)?([\da-z.-]+)\.([a-z.]{2,6})([\\w .-]*)*\?$/.test(v) || 'Must be valid url',
+      ],
+      thresholdRules:[
+        v => !!v || 'Threshold is required',
+        v => (0 <= v && v <= 1) || 'Threshold must be between 0 and 1',
+      ],
+    },
+  }),
+  methods:{
+    submit(){
+      if (!this.$refs.form.validate()) return
+
+      this.$emit('submit', this.url, this.threshold, this.ignoreClassifiers)
+    },
+  }
+}
+</script>
